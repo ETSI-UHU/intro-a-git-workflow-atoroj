ca3f15a362924beb8e3a18805ec057ec5d9cd917
David Rico <davidriico00@gmail.com>
2025-03-07 12:38:16 +0100

    refactorización del método multiplicación

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -30,8 +30,7 @@ public class Calculadora {
                 System.out.println("Resultado: " + resultado);
                 break;
             case '*':
-                resultado = num1 * num2;
-                System.out.println("Resultado: " + resultado);
+            	multiplicacion(num1, num2);
                 break;
             case '/':
                 if (num2 != 0) {
@@ -47,4 +46,11 @@ public class Calculadora {
 
         scanner.close();
     }
+
+	private static void multiplicacion(double num1, double num2) {
+		double resultado;
+		resultado = num1 * num2;
+		System.out.println("Resultado: " + resultado);
+	}
+    
 }
